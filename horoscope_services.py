a8f84df9440bc5e318b6a1da3dfb90aec6ca1463
Iuri Teles <olaioo@yahoo.com.br>
2020-07-27 18:39:35 -0300

    Correção no request para o horoscopo

--- horoscope_services.py
+++ horoscope_services.py
@@ -4,5 +4,5 @@ import os
 base_url_horoscope = 'http://horoscope-api.herokuapp.com'
 
 def get_horoscope(sign, scope):
-    response = requests.get(base_url_horoscope + '/horoscope/today/' + sign)
+    response = requests.get(base_url_horoscope + '/horoscope/today/' + sign).json()
     return response['horoscope']
