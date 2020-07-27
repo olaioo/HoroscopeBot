3c8f053df6e2462c4ce4212b1361dace67972eed
Iuri Teles <olaioo@yahoo.com.br>
2020-07-27 19:46:07 -0300

    Testando método get_horoscope

--- actions.py
+++ actions.py
@@ -17,6 +17,8 @@ def get_horoscope(parameters, return_var):
     sign = parameters['signo']
     scope = parameters['escopo']
 
+    res = horoscope_services.get_horoscope(sign, scope)
+
     return {
-        return_var: '' + horoscope_services.get_horoscope(sign, scope) 
+        return_var: "teste" 
     }
