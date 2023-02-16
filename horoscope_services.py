f124ca02e0d165c783736153cf44b4f33267f8ee
olaioo <olaioo@yahoo.com.br>
2023-02-16 22:11:19 +0000

    Fixing get sign function

--- horoscope_services.py
+++ horoscope_services.py
@@ -1,9 +1,10 @@
 import requests
 import os
 
+from datetime import datetime
+
 base_url_horoscope = 'http://horoscope-api.herokuapp.com'
 base_url_lovecalculator = 'https://love-calculator.p.rapidapi.com'
-base_url_zodiacsign = 'https://zodiac-sign.p.rapidapi.com'
 
 key_rapidapi = os.environ.get('RAPIDAPI_KEY')
 
@@ -16,5 +17,31 @@ def get_percentage(fname, sname):
     return response['result'] + " " + response['percentage'] + '%'
 
 def get_sign(date):
-    response = requests.get(base_url_zodiacsign + '/sign', {'date': date}, headers={'x-rapidapi-key': key_rapidapi})
-    return response.text
+    inner_date = datetime.fromisoformat(date)
+    month = inner_date.month * 100
+    if month < 120 :
+        return 'Capricorn'
+    elif month < 219 :
+        return 'Aquarius'
+    elif month < 321 :
+        return 'Pisces'
+    elif month < 420 :
+        return 'Aries'
+    elif month < 521 :
+        return 'Taurus'
+    elif month < 621 :
+        return 'Gemini'
+    elif month < 723 :
+        return 'Cancer'
+    elif month < 823 :
+        return 'Leo'
+    elif month < 923 :
+        return 'Virgo'
+    elif month < 1023 :
+        return 'Libra'
+    elif month < 1122 :
+        return 'Scorpio'
+    elif month < 1222 :
+        return 'Sagittarius'
+    else :
+        return None
